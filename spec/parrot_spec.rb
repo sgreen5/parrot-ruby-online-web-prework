564d1c45b4dca7d3150fbc1822f54d19b0d3d806
Shemmyla Green <shemmyla@hotmail.com>
2019-04-18 04:33:59 +0000

    Done.

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -5,21 +5,38 @@ describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
     expect($stdout).to receive(:puts).with("Squawk!")
     parrot
+    
+  def parrot
+    noise = "Squawk"
+    put noise
   end
 
   it 'should return the default phrase, "Squawk!" when called without any arguments' do
     phrase = parrot
     expect(phrase).to eq("Squawk!")
+    
+    def parrot
+      phrase (noise = "Squawk!")
+      put #{noise}
   end
 
   it 'should output the given phrase when called with an argument' do
     expect($stdout).to receive(:puts).with("Pretty bird!")
     parrot("Pretty bird!")
+    
+    def parrot
+      phrase "Pretty bird!"
+      put #{phrase}
   end
 
   it 'should return the given phrase when called with an argument' do
     phrase = parrot("Pretty bird!")
 
     expect(phrase).to eq("Pretty bird!")
+    
+    def phrase (parrot)
+      puts parrot 
+      
+      
   end
 end
